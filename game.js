d7f8b2a1f56534542652bcaec9d665b3755523f9
aiwinbenny <aiwinbenny33@gmail.com>
2020-11-23 23:58:50 +0530

    Added js events to display "X" and "O" at alternate clicks.

---
+++ game.js
@@ -0,0 +1,97 @@
+var c1=document.querySelector("#c1")
+var c2=document.querySelector("#c2")
+var c3=document.querySelector("#c3")
+var c4=document.querySelector("#c4")
+var c5=document.querySelector("#c5")
+var c6=document.querySelector("#c6")
+var c7=document.querySelector("#c7")
+var c8=document.querySelector("#c8")
+var c9=document.querySelector("#c9")
+
+var play=0
+
+
+c1.addEventListener("click", function () {
+  if(play%2 === 0)
+  {
+    c1.textContent ="X"
+  }
+  else{
+    c1.textContent = "O"
+  }
+  play++;
+})
+
+c2.addEventListener("click", function () {
+  if(play%2 === 0)
+  {
+    c2.textContent ="X"
+  }
+  else{
+    c2.textContent = "O"
+  }
+  play++;
+})
+c3.addEventListener("click", function () {
+  if(play%2 === 0)
+  {
+    c3.textContent ="X"
+  }
+  else{
+    c3.textContent = "O"
+  }play++;
+})
+c4.addEventListener("click", function () {
+  if(play%2 === 0)
+  {
+    c4.textContent ="X"
+  }
+  else{
+    c4.textContent = "O"
+  }play++;
+})
+c5.addEventListener("click", function () {
+  if(play%2 === 0)
+  {
+    c5.textContent ="X"
+  }
+  else{
+    c5.textContent = "O"
+  }play++;
+})
+c6.addEventListener("click", function () {
+  if(play%2 === 0)
+  {
+    c6.textContent ="X"
+  }
+  else{
+    c6.textContent = "O"
+  }play++;
+})
+c7.addEventListener("click", function () {
+  if(play%2 === 0)
+  {
+    c7.textContent ="X"
+  }
+  else{
+    c7.textContent = "O"
+  }play++;
+})
+c8.addEventListener("click", function () {
+  if(play%2 === 0)
+  {
+    c8.textContent ="X"
+  }
+  else{
+    c8.textContent = "O"
+  }play++;
+})
+c9.addEventListener("click", function () {
+  if(play%2 === 0)
+  {
+    c9.textContent ="X"
+  }
+  else{
+    c9.textContent = "O"
+  }play++;
+})
